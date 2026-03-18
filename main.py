172d4fa86b59c1299b577097363c4c08f349604c
Kirill <kirillpozdin0@gmail.com>
2026-03-19 01:09:05 +0300

    Changes: - Structuring of the code - Adding a new function for generating a theme from AI with user selection

--- main.py
+++ main.py
@@ -3,13 +3,26 @@ import os
 import telebot
 from dotenv import load_dotenv
 from database import save_request, init_db, get_last_topics, add_favorite, get_favorites, delete_last_favorite
-from keyboard import (get_main_inline_keyboard, get_uniq_inline_keyboard, get_back_inline_keyboard,
-                      get_only_back_keyboard, get_uniq_result_keyboard, get_favorites_keyboard,
-                      get_favorites_with_delete_keyboard, get_uniq_result_saved_keyboard)
+from keyboard import *
+from texts import *
+from states import *
 from promts import system_prompt_uniq, system_prompt_help, system_prompt_idea, base_system_prompt
-from ollama import ask_gemma
+from utils import build_prompt, safe_gemma, show_generation_message
 import telebot.apihelper as apihelper
 import time
+from session_data import *
+from generation_service import (
+    generate_smart_topic,
+    regenerate_smart_topic,
+    make_topic_easier,
+    make_topic_more_interesting,
+    generate_classic_topic
+)
+from favorites_service import (
+    build_favorites_view,
+    delete_last_favorite_and_build_view,
+    get_saved_result_keyboard_by_mode
+)
 
 # инициализация
 load_dotenv()
@@ -18,63 +31,8 @@ TOKEN = os.getenv("TOKEN")
 bot = telebot.TeleBot(TOKEN, parse_mode="Markdown")
 init_db()
 
-# состояния и хранение
-user_last_uniq_topic = {}
-user_last_menu = {}
-user_states = {}
-user_active_message = {}
-
-
 apihelper.CONNECT_TIMEOUT = 30
 apihelper.READ_TIMEOUT = 30
-
-
-# ии/промт
-def build_prompt(system_prompt, user_text):
-    return (
-            base_system_prompt
-            + "\n\n"
-            + system_prompt
-            + "\n\nЗАПРОС ПОЛЬЗОВАТЕЛЯ:\n"
-            + (user_text or "")
-    )
-
-
-def safe_gemma(prompt):
-    try:
-        response = ask_gemma(prompt)
-        if not response or not response.strip():
-            return "⚠️ *Не удалось получить ответ.*"
-        return response.strip()
-    except Exception as e:
-        print("Ошибка Ollama:", e)
-        return "❌ *ИИ временно недоступна.*"
-
-
-# уи текст
-def main_menu_text(user):
-    name = user.first_name
-
-    return (
-        f"👋 *Привет, {name}, добро пожаловать в главное меню.*\n\n"
-        "✨ Я помогаю с идеями проектов и анализом твоего проекта.\n\n"
-        "📌 Что я умею:\n"
-        "• 📝 придумать тему по запросу\n"
-        "• 🎲 сгенерировать случайную идею\n"
-        "• 🧠 помочь доработать тему\n\n"
-        "👇 Выбирай действие:"
-    )
-
-
-def show_generation_message(chat_id, msg_id):
-    try:
-        bot.edit_message_text(
-            chat_id=chat_id,
-            message_id=msg_id,
-            text="⏳ *Генерация ответа...*"
-        )
-    except:
-        pass
 
 
 # обработчик старта
@@ -106,9 +64,10 @@ def handle_callbacks(call):
 
     user_active_message[user_id] = msg_id
 
-    # ================== кнопка назад ==================
+# кнопка назад
     if data == "back_to_menu":
         user_states[user_id] = None
+        user_data[user_id] = {}
 
         try:
             bot.edit_message_text(
@@ -120,32 +79,23 @@ def handle_callbacks(call):
         except Exception as e:
             print("Ошибка back_to_menu:", e)
 
-    elif data == "back_to_uniq":
-        user_states[user_id] = "uniq"
 
+    elif data == "back_to_uniq":
+        user_states[user_id] = None
+        user_data[user_id] = {}
         try:
             bot.edit_message_text(
                 chat_id=chat_id,
                 message_id=msg_id,
-                text=(
-                    "🎯 *Генерация уникальной темы проекта*\n\n"
-                    "Я подберу *простую и реализуемую тему*, "
-                    "которую можно без проблем разработать и защитить.\n\n"
-                    "⏳ После нажатия кнопки *«🎲 Сгенерировать»* "
-                    "ответ может появиться не сразу — обычно это занимает "
-                    "*10–20 секунд*.\n\n"
-                    "👇 Доступные действия:\n"
-                    "• 🎲 *Сгенерировать* — получить новую уникальную тему\n"
-                    "• ⬅️ *Назад* — вернуться в главное меню"
-                ),
-                reply_markup=get_uniq_inline_keyboard()
+                text=uniq_mode_text(),
+                reply_markup=get_uniq_mode_keyboard()
             )
         except Exception as e:
             print("Ошибка back_to_uniq:", e)
 
-    elif data == "delete_last_favorite":
-        deleted = delete_last_favorite(user_id)
 
+    elif data == "delete_last_favorite":
+        text, keyboard, deleted = delete_last_favorite_and_build_view(user_id, delete_last_favorite, get_favorites)
         if not deleted:
             bot.answer_callback_query(
                 call.id,
@@ -153,18 +103,6 @@ def handle_callbacks(call):
                 show_alert=True
             )
             return
-
-        favorites = get_favorites(user_id)
-
-        if not favorites:
-            text = "⭐ *Последняя тема удалена.*\n\nУ тебя больше нет избранных тем."
-            keyboard = get_favorites_keyboard()
-        else:
-            text = "⭐ *Последняя тема удалена.*\n\n" + "\n\n".join(
-                f"{i + 1}. {t}" for i, t in enumerate(favorites)
-            )
-            keyboard = get_favorites_with_delete_keyboard()
-
         try:
             bot.edit_message_text(
                 chat_id=chat_id,
@@ -175,17 +113,17 @@ def handle_callbacks(call):
         except Exception as e:
             print("Ошибка delete_favorite:", e)
 
-    # ================== кнопка еще раз ==================
+# кнопка ещё раз
     elif data == "repeat":
         last = user_last_menu.get(user_id)
 
         if last == "menu_idea":
             user_states[user_id] = "awaiting_idea"
-            text = "📝 *Опиши, с чем должен быть связан проект* 👇"
+            text = repeat_idea_text()
 
         elif last == "menu_help":
             user_states[user_id] = "awaiting_help"
-            text = "🧠 *Опиши тему или задай вопрос* 👇"
+            text = repeat_help_text()
 
         else:
             return
@@ -200,53 +138,187 @@ def handle_callbacks(call):
         except Exception as e:
             print("Ошибка repeat:", e)
 
-    # ================== генерация уникальной темы ==================
+# генерация уникальной темы
     elif data == "menu_uniq":
+        user_states[user_id] = None
+        user_data[user_id] = {}
+
+        try:
+            bot.edit_message_text(
+                chat_id=chat_id,
+                message_id=msg_id,
+                text=uniq_mode_text(),
+                reply_markup=get_uniq_mode_keyboard()
+            )
+        except Exception as e:
+            print("Ошибка menu_uniq:", e)
+
+    elif data == "uniq_mode_classic":
         user_states[user_id] = "uniq"
 
         try:
             bot.edit_message_text(
                 chat_id=chat_id,
                 message_id=msg_id,
-                text=(
-                    "🎯 *Генерация уникальной темы проекта*\n\n"
-                    "Я подберу *простую и реализуемую тему*, "
-                    "которую можно без проблем разработать и защитить.\n\n"
-                    "⏳ После нажатия кнопки *«🎲 Сгенерировать»* "
-                    "ответ может появиться не сразу — обычно это занимает "
-                    "*10–20 секунд*.\n\n"
-                    "👇 Доступные действия:\n"
-                    "• 🎲 *Сгенерировать* — получить новую уникальную тему\n"
-                    "• ⬅️ *Назад* — вернуться в главное меню"
-                ),
+                text=uniq_classic_text(),
                 reply_markup=get_uniq_inline_keyboard()
             )
         except Exception as e:
-            print("Ошибка menu_uniq:", e)
+            print("Ошибка uniq_mode_classic:", e)
 
-    elif data == "uniq_generate":
-        last_topics = get_last_topics(user_id, limit=30)
-        rules_block = ""
+    elif data == "uniq_mode_smart":
+        user_states[user_id] = STATE_SELECT_DIRECTION
+        user_data[user_id] = {}
 
-        if last_topics:
-            rules_block = (
-                    "\n\n⚠️ ПРАВИЛО ДЛЯ ГЕНЕРАЦИИ:\n"
-                    "• НЕ повторяй темы из списка ниже\n"
-                    "• Каждый новый проект должен быть принципиально другим по сути и типу\n"
-                    "• Старайся делать тему такой, чтобы студент сказал «Хочу это сделать!»\n"
-                    "\nЗАПРЕЩЁННЫЕ ПОВТОРЫ:\n"
-                    + "\n".join(f"{i + 1}. {t[:120]}" for i, t in enumerate(last_topics))
+        try:
+            bot.edit_message_text(
+                chat_id=chat_id,
+                message_id=msg_id,
+                text=uniq_smart_start_text(),
+                reply_markup=get_direction_keyboard()
+            )
+        except Exception as e:
+            print("Ошибка uniq_mode_smart:", e)
+
+
+    elif data.startswith("dir_"):
+        user_states[user_id] = save_direction(user_id, data)
+        try:
+            bot.edit_message_text(
+                chat_id=chat_id,
+                message_id=msg_id,
+                text=uniq_step_project_type_text(),
+                reply_markup=get_project_type_keyboard()
             )
 
-        show_generation_message(chat_id, msg_id)
+        except Exception as e:
 
-        response = safe_gemma(
-            build_prompt(system_prompt_uniq + rules_block, "")
-        )
+            print("Ошибка выбора направления:", e)
 
-        user_last_uniq_topic[user_id] = response
-        save_request(user_id, "uniq", "", response)
 
+    elif data.startswith("type_"):
+        user_states[user_id] = save_project_type(user_id, data)
+        try:
+            bot.edit_message_text(
+                chat_id=chat_id,
+                message_id=msg_id,
+                text=uniq_step_duration_text(),
+                reply_markup=get_duration_keyboard()
+            )
+        except Exception as e:
+            print("Ошибка выбора типа проекта:", e)
+
+
+    elif data.startswith("time_"):
+        user_states[user_id] = save_duration(user_id, data)
+        try:
+            bot.edit_message_text(
+                chat_id=chat_id,
+                message_id=msg_id,
+                text=uniq_step_visual_text(),
+                reply_markup=get_visual_keyboard()
+            )
+        except Exception as e:
+            print("Ошибка выбора сроков:", e)
+
+
+    elif data.startswith("visual_"):
+        user_states[user_id] = save_visual(user_id, data)
+        try:
+            bot.edit_message_text(
+                chat_id=chat_id,
+                message_id=msg_id,
+                text=uniq_step_digital_text(),
+                reply_markup=get_digital_keyboard()
+            )
+        except Exception as e:
+            print("Ошибка выбора визуального результата:", e)
+
+
+
+    elif data.startswith("digital_"):
+        save_digital(user_id, data)
+        show_generation_message(bot, chat_id, msg_id)
+        response = generate_smart_topic(user_id, get_last_topics, build_prompt, base_system_prompt, system_prompt_uniq,
+                                        build_smart_uniq_prompt, safe_gemma, save_request)
+        try:
+            bot.edit_message_text(
+                chat_id=chat_id,
+                message_id=msg_id,
+                text=response,
+                reply_markup=get_smart_result_keyboard()
+            )
+        except Exception as e:
+            print("Ошибка smart generate:", e)
+        if not response.startswith("⚠️") and not response.startswith("❌"):
+            user_states[user_id] = None
+
+
+    elif data == "smart_regenerate":
+        show_generation_message(bot, chat_id, msg_id)
+        response = regenerate_smart_topic(user_id, get_last_topics, build_prompt, base_system_prompt,
+                                          system_prompt_uniq, build_smart_uniq_prompt, safe_gemma, save_request)
+        try:
+            bot.edit_message_text(
+                chat_id=chat_id,
+                message_id=msg_id,
+                text=response,
+                reply_markup=get_smart_result_keyboard()
+            )
+        except Exception as e:
+            print("Ошибка smart_regenerate:", e)
+
+
+    elif data == "make_easier":
+        last_topic = user_last_uniq_topic.get(user_id)
+        if not last_topic:
+            bot.answer_callback_query(
+                call.id,
+                text="❗ Нет темы для изменения",
+                show_alert=True
+            )
+            return
+        show_generation_message(bot, chat_id, msg_id)
+        response = make_topic_easier(user_id, build_prompt, base_system_prompt, system_prompt_uniq, build_easier_prompt,
+                                     safe_gemma, save_request)
+        try:
+            bot.edit_message_text(
+                chat_id=chat_id,
+                message_id=msg_id,
+                text=response,
+                reply_markup=get_smart_result_keyboard()
+            )
+        except Exception as e:
+            print("Ошибка make_easier:", e)
+
+
+    elif data == "make_more_interesting":
+        last_topic = user_last_uniq_topic.get(user_id)
+        if not last_topic:
+            bot.answer_callback_query(
+                call.id,
+                text="❗ Нет темы для изменения",
+                show_alert=True
+            )
+            return
+        show_generation_message(bot, chat_id, msg_id)
+        response = make_topic_more_interesting(user_id, build_prompt, base_system_prompt, system_prompt_uniq,
+                                               build_more_interesting_prompt, safe_gemma, save_request)
+        try:
+            bot.edit_message_text(
+                chat_id=chat_id,
+                message_id=msg_id,
+                text=response,
+                reply_markup=get_smart_result_keyboard()
+            )
+        except Exception as e:
+            print("Ошибка make_more_interesting:", e)
+
+
+    elif data == "uniq_generate":
+        show_generation_message(bot, chat_id, msg_id)
+        response = generate_classic_topic(user_id, get_last_topics, build_prompt, base_system_prompt,
+                                          system_prompt_uniq, safe_gemma, save_request)
         try:
             bot.edit_message_text(
                 chat_id=chat_id,
@@ -275,24 +347,20 @@ def handle_callbacks(call):
         )
 
         try:
+            mode = user_last_uniq_mode.get(user_id)
+            keyboard = get_saved_result_keyboard_by_mode(mode)
+
             bot.edit_message_reply_markup(
                 chat_id=chat_id,
                 message_id=msg_id,
-                reply_markup=get_uniq_result_saved_keyboard()
+                reply_markup=keyboard
             )
         except Exception as e:
             print("Ошибка add_favorite:", e)
 
+
     elif data == "show_favorites":
-        favorites = get_favorites(user_id)
-
-        if not favorites:
-            text = "⭐ *У тебя пока нет избранных тем*"
-            keyboard = get_favorites_keyboard()
-        else:
-            text = "⭐ *Твои избранные темы:*\n\n" + "\n\n".join(f"{i + 1}. {t}" for i, t in enumerate(favorites))
-            keyboard = get_favorites_with_delete_keyboard()
-
+        text, keyboard = build_favorites_view(user_id, get_favorites)
         try:
             bot.edit_message_text(
                 chat_id=chat_id,
@@ -303,7 +371,7 @@ def handle_callbacks(call):
         except Exception as e:
             print("Ошибка show_favorites:", e)
 
-    # ================== тема по запросу ==================
+# тема по запросу
     elif data == "menu_idea":
         user_states[user_id] = "awaiting_idea"
         user_last_menu[user_id] = "menu_idea"
@@ -312,21 +380,13 @@ def handle_callbacks(call):
             bot.edit_message_text(
                 chat_id=chat_id,
                 message_id=msg_id,
-                text=(
-                    "📝 *Тема проекта по твоему запросу*\n\n"
-                    "Опиши, с чем должен быть связан проект: предмет, направление, "
-                    "интересующую область или примерную идею.\n\n"
-                    "⏳ После отправки сообщения ответ может появиться не сразу — "
-                    "обычно генерация занимает *10–20 секунд*.\n\n"
-                    "⬅️ *Назад* — вернуться в главное меню.\n\n"
-                    "👇 Напиши свой запрос:"
-                ),
+                text=menu_idea_text(),
                 reply_markup=get_only_back_keyboard()
             )
         except Exception as e:
             print("Ошибка menu_idea:", e)
 
-    # ================== помощь ==================
+# помощь
     elif data == "menu_help":
         user_states[user_id] = "awaiting_help"
         user_last_menu[user_id] = "menu_help"
@@ -335,15 +395,7 @@ def handle_callbacks(call):
             bot.edit_message_text(
                 chat_id=chat_id,
                 message_id=msg_id,
-                text=(
-                    "🧠 *Помощь с темой проекта*\n\n"
-                    "Напиши тему проекта или задай вопрос — я помогу уточнить формулировку, "
-                    "улучшить идею или подсказать, как её доработать.\n\n"
-                    "⏳ Ответ формируется не мгновенно — "
-                    "обычно это занимает *10–20 секунд*.\n\n"
-                    "⬅️ *Назад* — вернуться в главное меню.\n\n"
-                    "👇 Опиши тему или задай вопрос:"
-                ),
+                text=menu_help_text(),
                 reply_markup=get_only_back_keyboard()
             )
         except Exception as e:
@@ -363,18 +415,18 @@ def handle_text(message):
         return
 
     if state == "awaiting_idea":
-        show_generation_message(message.chat.id, msg_id)
+        show_generation_message(bot, message.chat.id, msg_id)
 
         response = safe_gemma(
-            build_prompt(system_prompt_idea, message.text)
+            build_prompt(base_system_prompt, system_prompt_idea, message.text)
         )
         save_request(user_id, "idea", message.text, response)
 
     elif state == "awaiting_help":
-        show_generation_message(message.chat.id, msg_id)
+        show_generation_message(bot, message.chat.id, msg_id)
 
         response = safe_gemma(
-            build_prompt(system_prompt_help, message.text)
+            build_prompt(base_system_prompt, system_prompt_help, message.text)
         )
         save_request(user_id, "help", message.text, response)
 
